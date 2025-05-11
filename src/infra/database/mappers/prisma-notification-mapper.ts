7366ba877139af7084ffdcd6613712ae4e1fb211
Leonardo Rodrigues <ldrigues.dev@gmail.com>
2025-05-11 13:59:08 -0300

    feat: add completed repository prisma

--- src/infra/database/mappers/prisma-notification-mapper.ts
+++ src/infra/database/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
-import type { Notification } from "@domain/notification/application/entities/notification";
+import { Content } from '@domain/notification/application/entities/content';
+import { Notification } from '@domain/notification/application/entities/notification';
+import { Notification as RawNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -9,6 +11,19 @@ export class PrismaNotificationMapper {
       recipientId: notification.recipientId,
       readAt: notification.readAt,
       createdAt: notification.createdAt,
-    }
+    };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
